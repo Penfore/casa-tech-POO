6db954b64b3436113e8651d5a2f3b853bceeb861
Davi Emanuel <daviemanuelll04@gmail.com>
2021-10-16 02:06:58 -0300

    Telas Estoque, Clientes, e ajuste na tela Login.

---
+++ src/view/Estoque.java
@@ -0,0 +1,27 @@
+package src.view;
+
+import javafx.application.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Estoque extends Application{
+
+	public static void main(String[] args) {
+		launch();
+
+	}
+
+	@Override
+	public void start(Stage ps) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("telas/estoque.fxml"));
+		
+		Scene cena = new Scene(root);
+		
+		ps.setTitle("casatech-estoque");
+		ps.setScene(cena);
+		ps.show();
+	}
+
+}
